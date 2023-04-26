29a5a8a9d57d2b4cf7343d0e15368204efa83e2b
SadoninS <stanislavsadonin@gmail.com>
2023-04-26 22:41:12 +0400

    add burger-menu

---
+++ js/burger.js
@@ -0,0 +1,24 @@
+const BURGER_OPENED_CLASSNAME = 'burger-open';
+const BURGER_BTN_OPENED_CLASSNAME = 'burger-close-btn';
+const BODY_FIXED_CLASSNAME_2 = 'body-fixed-2';
+
+const bodyNode2 = document.querySelector('body');
+const burgerNode = document.querySelector('.js-burger');
+const burgerBtnNode = document.querySelector('.js-burger-btn');
+const burgerContentNode = document.querySelector('.js-burger-content')
+
+burgerBtnNode.addEventListener('click', toggleBurger);
+
+burgerNode.addEventListener('click', (event) => {
+    const isClickOutsideContent = !event.composedPath().includes(burgerContentNode)
+
+    if (isClickOutsideContent) {
+        toggleBurger();
+    }
+})
+
+function toggleBurger() {
+    burgerNode.classList.toggle(BURGER_OPENED_CLASSNAME);
+    bodyNode2.classList.toggle(BODY_FIXED_CLASSNAME_2);
+    burgerBtnNode.classList.toggle(BURGER_BTN_OPENED_CLASSNAME);
+}
